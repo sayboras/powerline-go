fd4817957cf08de9986d8408f1fc51bde9f2d3f4
sayboras <sayboras@yahoo.com>
2020-01-26 09:58:27 +1100

    Added support for aws account

---
+++ segment-aws-account.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func segmentAWSAccount(p *powerline) {
+	account, err := exec.Command("aws", "sts get-caller-identity", "--output json", "--query 'Account'").CombinedOutput()
+	if err != nil {
+		return
+	}
+
+	if len(account) > 0 {
+		p.appendSegment("aws", segment{
+			content:    strings.TrimSuffix(strings.Replace(string(account), "\"", "", 1), "\n"),
+			foreground: p.theme.AWSFg,
+			background: p.theme.AWSBg,
+		})
+	}
+}
